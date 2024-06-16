7e0091659ae4d7c6ef3180d96a02d905f28733e4
lichong <git@lichong.host>
2024-06-16 23:06:44 +0800

    init

---
+++ common/core/src/test/java/com/eoi/portal/server/core/CoreApplicationTests.java
@@ -0,0 +1,17 @@
+package com.eoi.portal.server.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+@SpringBootConfiguration
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assert.isTrue(true, "true");
+    }
+
+}
